5c4f3d997f6c3944df013864ed892c7fb610c485
TillusB <tbalbach@gmx.net>
2015-10-16 16:16:56 +0200

    first iteration - change to 1 room per frame

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Generator : MonoBehaviour {
+    public List<Room> Rooms;
+    public GameObject Base;
+    private float height;
+    private float width;
+    private float x;
+    private float y;
+
+    // Use this for initialization
+    void Start () {
+        height = Base.transform.localScale.y;
+        width = Base.transform.localScale.x;
+        Test(5, Base);
+        x = -width / 2;
+        y = -height / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkNextPos();
+	}
+
+
+
+    void Test(int i, GameObject Base)
+    {
+        int roomscreated = 0;
+        while(roomscreated < i)
+        {
+            var room = GameObject.Instantiate(Rooms[Random.Range(0, Rooms.Count - 1)]);
+            findUnoccupiedPos(room);
+            roomscreated++;
+        }
+    }
+
+
+    private void findUnoccupiedPos(Room toBePlaced)
+    {
+        toBePlaced.gameObject.transform.position = new Vector3(-width / 2, -height / 2, 0);
+        for(float y = -height/2; y <= height/2; y++)
+        {
+            for(float x = -width/2; x <= width/2; x++)
+            {
+
+                Debug.DrawLine(Vector3.zero, toBePlaced.transform.position);
+                toBePlaced.transform.position = new Vector3(x, y, 0);
+                if (toBePlaced.free)
+                {
+                    return;
+                }
+                else continue;
+            }
+        }
+
+        if (!toBePlaced.free) Destroy(toBePlaced.gameObject);
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+    public bool free = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

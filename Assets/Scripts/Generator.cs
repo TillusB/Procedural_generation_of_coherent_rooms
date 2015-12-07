8545c7f0c40ab7196682d596272a910883bec220
TillusB <tbalbach@gmx.net>
2015-12-07 17:07:55 +0100

    Rooms can be added and get neighbours. Very basic

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,15 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Generator : MonoBehaviour {
-
+    private GameObject defaultRoom;
+    public Room room;
     // Use this for initialization
     void Start () {
-
+        Room room1 = CreateRoom(new Vector3(2, 2, 2), new Vector3(0, 0, 2));
+        Room room2 = CreateRoom(new Vector3(1, 1, 1), new Vector3(0, 0, 0));
+        room1.AddNeighbour(room2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+    public Room CreateRoom(Vector3 size, Vector3 pos)
+    {
+        defaultRoom = new GameObject();
+        defaultRoom.AddComponent<Room>();
+        room = defaultRoom.GetComponent<Room>();
+
+        room.size = size;
+        room.position = pos;
+        return room;
+    }
 
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,12 +2,40 @@
 using System.Collections;
 
 public class Room : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
+
+    private System.Collections.Generic.Dictionary<string, int> roomType = new System.Collections.Generic.Dictionary<string, int>() {
+        {"Public Room", 100},
+        {"Private Room", 1},
+        {"Hallway", 100},
+    };
+
+    public Vector3 size = new Vector3 (1,1,1);
+    public Vector3 position = new Vector3 (0,0,0);
+    public System.Collections.Generic.List<Room> neighbours = new System.Collections.Generic.List<Room>();
+
+    void Start () {
+        transform.parent = GameObject.Find("Rooms").transform;
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.AddComponent<BoxCollider>();
+        gameObject.transform.position = position;
+        gameObject.transform.localScale = size;
+    }
+
+    void Update () {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void AddNeighbour(Room newNeighbour)
+    {
+        neighbours.Add(newNeighbour);
+        if (!newNeighbour.neighbours.Contains(this))
+        {
+            newNeighbour.AddNeighbour(this);
+        }
+        else return;
+    }
+
+    public void SetRoomType(string type)
+    {
+
+    }
 }
